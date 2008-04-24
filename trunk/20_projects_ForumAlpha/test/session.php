b24a4e76d4990efd5eb983d32dfe704ec151a3ba
andre13111984 <andre13111984@e78d9b13-884b-0410-811d-8af581424660>
2008-04-24 17:55:39 +0000

    erstes mal upgeloaded !

---
+++ trunk/20_projects_ForumAlpha/test/session.php
@@ -0,0 +1,8 @@
+<?php
+						echo '&bull;SESSION.php # error_reporting(0);<br>';
+error_reporting(0);
+						echo '&bull;SESSION.php # session_start();<br>';
+session_start();
+						echo '&bull;SESSION.php # session_set_cookie_params(20);<br>';
+session_set_cookie_params(20);
+?>
